d8d5e6e7f9f6787af1c73d33c3285626e21db4ba
Pavel Ditenbir <pavel.ditenbir@gmail.com>
2021-01-03 19:12:05 +0700

    Add REST_API_PAGE_SIZE to config

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ A tool that retrieves information about LEGO parts
 # Example of config.ini
 ```
 [debug]
+RESTApiPageSize=10
 MaxRESTApiRequests=2 # to limit the number of requests to the server 
 
 [rebrickable.com]
--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import configparser
 import sys
 
-MAX_REST_API_REQUESTS = sys.maxsize
 TOKEN = ''
+MAX_REST_API_REQUESTS = sys.maxsize
+REST_API_PAGE_SIZE = 100
 
 
 def init_config():
@@ -13,10 +14,14 @@ def init_config():
         print('WARNING Cannot read config.ini file with settings')
         return
 
-    global MAX_REST_API_REQUESTS
-    if 'debug' in config and 'MaxRESTApiRequests' in config['debug']:
-        MAX_REST_API_REQUESTS = config['debug']['MaxRESTApiRequests']
-
     global TOKEN
     if 'rebrickable.com' in config and 'token' in config['rebrickable.com']:
         TOKEN = config['rebrickable.com']['token']
+
+    global MAX_REST_API_REQUESTS
+    if 'debug' in config and 'MaxRESTApiRequests' in config['debug']:
+        MAX_REST_API_REQUESTS = int(config['debug']['MaxRESTApiRequests'])
+
+    global REST_API_PAGE_SIZE
+    if 'debug' in config and 'RESTApiPageSize' in config['debug']:
+        REST_API_PAGE_SIZE = int(config['debug']['RESTApiPageSize'])
